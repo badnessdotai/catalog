6e1e879bb039006e0d9e015ad5384fd6f86bd602
coderrg <typhoonrg@gmail.com>
2023-03-26 15:04:25 -0500

    Redesign entry page

--- app/catalog/[slug]/page.tsx
+++ app/catalog/[slug]/page.tsx
@@ -27,80 +27,88 @@ export default async function EntryPage({
 
   return (
     <article>
-      <div className="flex gap-8 w-full justify-end">
-        <div className="hidden lg:block lg:w-1/6" />
-        <h1 className="lg:w-5/6 justify-end font-serif text-4xl lg:text-7xl mt-64">
+      <div className="flex flex-col items-start mt-32">
+        <div className="font-mono text-lg lg:text-3xl truncate uppercase">
+          <span className="font-bold">
+            {entry.date.toLocaleDateString("en-US", {
+              day: "numeric",
+              month: "short",
+              year: "numeric",
+            })}
+          </span>
+        </div>
+        <h1 className="font-serif text-4xl lg:text-7xl mt-4">
           {entry.title}
         </h1>
       </div>
-      <hr className="my-4 lg:my-8" />
-      <div className="my-4 flex flex-col lg:flex-row gap-4 lg:gap-8 w-full">
-        <div className="lg:w-1/6">
-          <p className="font-mono text-lg lg:text-3xl truncate uppercase">
-            <span className="font-bold">
-              {entry.date.toLocaleDateString("en-US", {
-                day: "numeric",
-                month: "short",
-                year: "numeric",
-              })}
-            </span>
-          </p>
-        </div>
-        <div className="lg:w-5/6">
-          <p className="font-mono text-lg lg:text-3xl uppercase flex flex-wrap gap-4 lg:gap-16">
+      <div className="flex flex-col lg:flex-row items-start mt-24">
+        <div className="w-1/5 lg:w-1/6">
+          <div className="font-mono text-lg lg:text-3xl truncate uppercase">
             {entry.categories.length > 0 && (
-              <span className="font-bold flex flex-wrap gap-8">
-                {entry.categories.map((c) => (
-                  <Link key={c} href={`/categories/${c}`}>
-                    {c}
-                  </Link>
-                ))}
-              </span>
-            )}
-            {entry.models.length > 0 && (
-              <span className="font-bold flex flex-wrap gap-8">
-                {entry.models.map((c) => (
-                  <Link key={c} href={`/models/${c}`}>
-                    {c}
-                  </Link>
-                ))}
-              </span>
+              <div>
+                <p className="font-bold text-gray-400 mb-2">CATEGORIES</p>
+                <span className="font-bold flex flex-wrap gap-2">
+                  {entry.categories.map((c) => (
+                    <Link key={c} href={`/categories/${c}`}>
+                      {c}
+                    </Link>
+                  ))}
+                </span>
+              </div>
             )}
             {entry.companies.length > 0 && (
-              <span className="font-bold flex flex-wrap gap-8">
-                {entry.companies.map((c) => (
-                  <Link key={c} href={`/companies/${c}`}>
-                    {c}
-                  </Link>
-                ))}
-              </span>
+              <div>
+                <p className="font-bold text-gray-400 mb-2 mt-12">COMPANIES</p>
+                <span className="font-bold flex flex-wrap gap-2">
+                  {entry.companies.map((c) => (
+                    <Link key={c} href={`/companies/${c}`}>
+                      {c}
+                    </Link>
+                  ))}
+                </span>
+              </div>
             )}
-          </p>
-        </div>
-      </div>
-      <hr className="my-4 lg:my-8" />
-      <div className="flex gap-8 w-full mt-4">
-        <div className="hidden lg:block lg:w-1/6" />
-        <div
-          className="prose prose-xl lg:prose-2xl prose-black dark:prose-invert text-black dark:text-white"
-          dangerouslySetInnerHTML={{ __html: renderedMarkdown }}
-        />
-      </div>
-      {relatedEntries.length > 0 && (
-        <div className="flex gap-8 w-full mt-16">
-          <div className="hidden lg:block lg:w-1/6" />
-          <div className="lg:w-5/6">
-            <div className="font-mono uppercase text-xl mb-8">See Also</div>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-              {relatedEntries.slice(0, 3).map((e) => (
-                <div key={e.slug}>
-                  <EntryCard entry={e} withTopline={true} />
-                </div>
-              ))}
-            </div>
+            {entry.models.length > 0 && (
+              <div>
+                <p className="font-bold text-gray-400 mb-2 mt-12">MODELS</p>
+                <span className="font-bold flex flex-wrap gap-2">
+                  {entry.models.map((c) => (
+                    <Link key={c} href={`/models/${c}`}>
+                      {c}
+                    </Link>
+                  ))}
+                </span>
+              </div>
+            )}
           </div>
         </div>
-      )}
+        <div className="flex-1 w-full">
+          <div
+            className="text-4xl text-black dark:text-white mb-32"
+            dangerouslySetInnerHTML={{ __html: renderedMarkdown.replaceAll("</p>\n<p>", "</p><br><p>") }}
+          />
+        </div>
+      </div>
+      <div className="flex flex-col lg:flex-row items-start mt-24">
+        <div className="w-1/5 lg:w-1/6">
+        </div>
+        <div>
+          {relatedEntries.length > 0 && (
+            <div className="flex gap-8 w-full mt-16">
+              <div className="lg:w-5/6">
+                <div className="font-mono uppercase text-xl mb-8">See Also</div>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+                  {relatedEntries.slice(0, 3).map((e) => (
+                    <div key={e.slug}>
+                      <EntryCard entry={e} withTopline={true} />
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
     </article>
   );
 }
