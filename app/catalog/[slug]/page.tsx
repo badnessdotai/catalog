cd79f3df4261c7e2d13c0f87b2115f8340b19766
coderrg <typhoonrg@gmail.com>
2023-03-29 18:37:30 -0500

    Add info box explaining experimental for relevant examples

--- app/catalog/[slug]/page.tsx
+++ app/catalog/[slug]/page.tsx
@@ -107,6 +107,17 @@ export default async function EntryPage({
               __html: renderedMarkdown.replaceAll("</p>\n<p>", "</p><br><p>"),
             }}
           />
+          {entry.experimental && (
+            <div className="bg-gray-200 border-t-4 border-gray-500 rounded-b text-gray-900 px-4 py-3 shadow-md mt-16" role="alert">
+              <div className="flex">
+                <div className="py-1"><svg className="fill-current h-6 w-6 text-gray-500 mr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z" /></svg></div>
+                <div>
+                  <p className="font-bold">The example above is experimental.</p>
+                  <p className="text-sm">This means it was produced to demonstrate a real-world harm; although there was no actual harm caused in this setting, the example exhibits a potential risk.</p>
+                </div>
+              </div>
+            </div>
+          )}
           <div>
             {relatedEntries.length > 0 && (
               <div className="flex gap-8 w-full mt-16">
